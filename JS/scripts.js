af673b4642442b7edd9c136e6859b7acfed227a2
VitorFarias1 <vitor.rodriguesfarias@gmail.com>
2024-06-28 18:43:42 -0300

    wip: estilo pronto e lógica iniciada

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,32 @@
-console.log("teste")
+// Elements selection
+const multiplicationForm = document.querySelector('#multiplication-form')
+const numberInput= document.querySelector('#number')
+const multiplicationInput = document.querySelector('#multiplicator')
+const multiplicationTable = document.querySelector('#multiplication-operation');
+
+// Functions
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+
+        console.log(result)
+    }
+}
+
+
+// Events
+multiplicationForm.addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return
+
+    console.log(multiplicationNumber, multiplicatorNumber);
+
+    createTable(multiplicationNumber, multiplicatorNumber)
+})
